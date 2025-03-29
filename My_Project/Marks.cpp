7afc359eda1a1918e3c02426f4bdeccdc7c5ad05
LiuYun0820 <qw22033@icloud.com>
2025-03-29 18:03:07 +0800

    Update Marks.cpp

--- My_Project/Marks.cpp
+++ My_Project/Marks.cpp
@@ -6,4 +6,10 @@ void Marks()
 	std::cout << "\n";
 	for (int i = 3; i > 0; --i)
 		std::cout << line << std::endl;
+
+	std::cout << "Please enter any words to continue...\n" << ":" << std::flush;
+	std::cin.get();
+	int input = 0;
+	if (1)
+		std::cin >> input;
 }
